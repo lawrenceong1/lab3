98add3ac17f8a1ee59a7a5bbf9f50cb98084cb99
lawrenceong1 <thelawrenceong@gmail.com>
2023-12-01 17:34:52 -0800

    update these

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -25,7 +25,8 @@ public class ArrayExamples {
   // Returns a *new* array with all the elements of the input array in reversed
   // order
 
-  /* incorrect code
+  // incorrect code
+  
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
@@ -33,9 +34,11 @@ public class ArrayExamples {
     }
     return arr;
   }
-  */
+  
+  
 
   // Corrected code
+  /*
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
@@ -43,6 +46,8 @@ public class ArrayExamples {
     }
     return newArray;
   }
+  */
+
 
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
--- ArrayTests.java
+++ ArrayTests.java
@@ -2,35 +2,59 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
+  //
+  /*
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
+  */
 
+  //
+  /*
   @Test
   public void testReverseInPlace2() {
     int[] input1 = { 3, 4 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 4, 3 }, input1);
   }
+  */
 
+  //
+  
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  
 
+  //
+  /*
   @Test
   public void testReversed2() {
     int[] input1 = { 3 };
     assertArrayEquals(new int[]{ 3 }, ArrayExamples.reversed(input1));
   }
+  */
 
+  /*
+  @Test 
+	public void testReversed3() {
+    int[] input1 = { 3, 4, 5 };
+    int[] output = ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[]{ 5, 4, 3 }, output);
+	}
+  */
+
+  // 
+  /*
   @Test
   public void testAverage() {
     double[] input1 = { 3, 3, 4 };
     assertEquals(3.5, ArrayExamples.averageWithoutLowest(input1), 0.0001);
   }
+  */
 }
